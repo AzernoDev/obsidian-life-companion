3add8c66de758af8daf18404f158a87ff9fca17f
Zero Liu <zerolxy@gmail.com>
2024-12-16 23:31:39 -0800

    Add tailwind css token

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,11 @@
 export function App() {
-	return <div className="text-3xl font-bold underline">!Hello World</div>;
+	return (
+		<div className="underline text-normal border border-solid">
+			<div className="border border-solid m-10">Hello World</div>
+			<div className="text-icon-color-hover rounded-clickable-icon">
+				it
+			</div>
+			<div className="z-cover line-through text-text-italic">it</div>
+		</div>
+	);
 }
